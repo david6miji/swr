b8d289a73d077880a62f20ca89d241be48f09e72
david6miji <frog@falinux.com>
2016-03-03 23:50:55 +0900

    SWR 기본 표출 형태 및 얇은 네비바 설정함

--- swr/client/js/controller/nologin.js
+++ swr/client/js/controller/nologin.js
@@ -21,37 +21,6 @@ function ($scope,authServices ) {
   	};
 	
 	$('[data-toggle="tooltip"]').tooltip()
-	
-	console.log( 'window.document.location.host = ', window.document.location.host );
-//	var host = window.document.location.host.replace(/:.*/, '');
-	var host = window.document.location.host;
-	console.log( 'host = ', host );
-	
-	var elem = document.getElementById('pad');
-    var cm = CodeMirror.fromTextArea(elem, {
-        mode: "text/plain",
-		lineNumbers: true,
-		styleActiveLine: true,
-    });
-	
-	console.log( 'ppad with = ', $('#ppad').width());
-	
-    cm.setSize($('#ppad').width(), 600);
-	
-//	var ws = new WebSocket('ws://' + host + '/nologin/doodle'  );
-	var ws = new WebSocket('ws://' + host ,'doodle-protocol' );
-	var sjs = new window.sharejs.Connection(ws);
-	
-	var doc = sjs.get('nologin_doodle', '대문글');
-	
-	doc.subscribe();
-    doc.whenReady(function () {
-      if (!doc.type) doc.create('text');
-      if (doc.type && doc.type.name === 'text') {
-        doc.attachCodeMirror(cm);
-      }
-    });
-	
 	 
 	
 }]);  
