04cf65885e91588a17a51d3f05c119128ab2803c
david6miji <frog@falinux.com>
2016-03-07 17:25:58 +0900

    ssh2 npm 모듈 설치 및 시험

--- swr/client/js/controller/nologin.js
+++ swr/client/js/controller/nologin.js
@@ -9,17 +9,36 @@
  */
 var mainApp = angular.module('mainApp'); 
 mainApp.controller('nologinCtrl', 
-[ '$scope', 'authServices',
-function ($scope,authServices ) {
+[ '$scope', 'authServices', 'Test',
+function ($scope,authServices, Test ) {
 	
 	console.log( 'CALL nologinCtrl' );
 	
-	$scope.login = function(user){
-		 console.log( 'CALL $scope.login' );
-		 console.log( 'user = ', user );
-  	 	 authServices.login(user);
-  	};
+	$scope.info = {
+		host : "192.168.1.10",
+		port : 22,
+		username : "webconn",
+		password : "",
+	};
 	
+	$scope.connect = function(info){
+		 console.log( 'CALL $scope.connect' );
+		 console.log( 'info = ', info );
+//  	 	 authServices.login(user);
+
+        Test.connect( info ).$promise.then(function ( value,responseHeaders) {
+			console.log( value );
+			var data = value.data;
+			
+            if (typeof data== 'object') {
+				  data = JSON.stringify(data, undefined, 2);
+				  console.log( data );
+            }
+				  
+		});
+
+  	};
+
 	$('[data-toggle="tooltip"]').tooltip()
 	 
 	
--- swr/server/boot/test.js
+++ swr/server/boot/test.js
@@ -1,3 +1,5 @@
+var Client = require('ssh2').Client;
+
 module.exports = function(app) {
 	
     var ds = app.loopback.createDataSource('memory');
@@ -21,6 +23,61 @@ module.exports = function(app) {
         }
     );
 
+    Test.connect = function( info, cb) {
+    	console.log( 'Call Test.connect remote method - data = ', info );
+		
+		var conn = new Client();
+		
+		conn.on('ready', function() {
+			console.log( 'Event ready ' );
+			
+            conn.shell(function(err, stream) {
+				
+                if (err) throw err;
+                stream.on('close', function() {
+					
+                      console.log('Stream :: close');
+                      conn.end();
+				  
+                });
+				
+				stream.on('data', function(data) {
+					
+                      console.log('STDOUT: ' + data);
+				  
+                });
+				
+				stream.stderr.on('data', function(data) {
+                      console.log('STDERR: ' + data);
+                });
+				
+                stream.end('ls -l\nexit\n');
+				
+            });	
+
+		});
+
+		conn.connect({
+			host: info.host,
+			port: info.port,
+			username: info.username,
+			password: info.password
+		});
+
+   	    response = { ack : 'ok' };
+   	    cb(null, response);
+		
+    };
+    
+    Test.remoteMethod(
+        'connect',
+        {
+          http: {path: '/connect', verb: 'post'},
+          accepts: {arg: 'data', type: 'object', http: { source: 'body' } },
+          returns: {arg: 'data', type: 'object' }
+        }
+    );
+	
     app.model(Test);
 	
 };
